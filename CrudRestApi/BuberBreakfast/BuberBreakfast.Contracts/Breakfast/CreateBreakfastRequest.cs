db564c03aba71960d6aa3f8dc551e8d251f9e981
swdev77 <swdev77@gmail.com>
2023-01-16 22:48:36 +0500

    First commit

---
+++ CrudRestApi/BuberBreakfast/BuberBreakfast.Contracts/Breakfast/CreateBreakfastRequest.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+namespace BuberBreakfast.Contracts.Breakfast;
+
+public record CreateBreakfastRequest(
+    string Name,
+    string Description,
+    DateTime StartDateTime,
+    DateTime EndDateTime,
+    List<string> Savory,
+    List<string> Sweet);
---
+++ CrudRestApi/BuberBreakfast/Readme.md
@@ -0,0 +1,5 @@
+# CRUD REST API in 1 Hour using .NET
+
+from the [Amichai Mantinband](https://www.youtube.com/@amantinband) Youtube channel
+
+[Video](hhttps://youtu.be/vftu-pqdUzQ)
