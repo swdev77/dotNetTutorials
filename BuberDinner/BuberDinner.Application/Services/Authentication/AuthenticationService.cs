eca1ae931194d8b6fd252d9dc5ae1e0dbff0ffe9
swdev77 <swdev77@gmail.com>
2023-01-31 22:51:49 +0500

    Jwt token generator...

--- BuberDinner/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner/BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,7 +1,16 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+
 namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
     public AuthenticationResult Login(string email, string password)
     {
         return new AuthenticationResult(
@@ -12,13 +21,23 @@ public class AuthenticationService : IAuthenticationService
             "token");
     }
 
-    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+    public AuthenticationResult Register(
+        string firstName,
+        string lastName,
+        string email,
+        string password)
     {
+        //check if user exists
+        //create user if not exists
+
+        Guid userId = Guid.NewGuid();
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
         return new AuthenticationResult(
-            Guid.NewGuid(),
+            userId,
             firstName,
             lastName,
             email,
-            "token");
+            token);
     }
+
 }
---
+++ BuberDinner/BuberDinner.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,41 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Services;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BuberDinner.Infrastructure.Authentication;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider)
+    {
+        _dateTimeProvider = dateTimeProvider;
+    }
+
+    public string GenerateToken(Guid userId, string firstName, string lastName)
+    {
+        var signingCredentials = new SigningCredentials(
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key")),
+            SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+            new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+            new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        var securityToken = new JwtSecurityToken(
+            issuer: "BuberDinner",
+            claims: claims,
+            expires: _dateTimeProvider.UtcNow.AddHours(1),
+            signingCredentials: signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(securityToken);
+    }
+}
