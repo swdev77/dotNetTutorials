b5685e370a124c633a2550abd6733333dfd794af
swdev77 <swdev77@gmail.com>
2023-03-22 20:00:18 +0500

    View Models ...

---
+++ MauiBuberBreakfast/ViewModels/MyBreakfastViewModel.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MauiBuberBreakfast.Models;
+
+namespace MauiBuberBreakfast.ViewModels;
+
+public partial class MyBreakfastViewModel : ObservableObject
+{
+    [ObservableProperty]
+    List<Breakfast> breakfasts;
+
+    public MyBreakfastViewModel()
+    {
+        LoadBreakfasts();
+    }
+
+    private void LoadBreakfasts()
+    {
+        Breakfasts = new()
+        {
+            new Breakfast(
+                        Name: "Vegan Sunshine",
+                        Description: "Vegan everthing! Join us for a healthy breakfas full of vegan dishes and Cookies",
+                        StartDateTime: DateTime.Now.AddDays(1),
+                        EndDateTime: DateTime.Now.AddDays(1).AddHours(2),
+                        Image: new Uri("https://images.unsplash.com/photo-1659984716295-c3d4be94dc4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80"),
+                        Savory: new List<string> { "Oatmeal", "Advocado toast", "Omelet", "Salad" },
+                        Sweet: new List<string> { "Cookie" }),
+            new Breakfast(
+                        Name: "Breakfast 0 Tiffany's",
+                        Description: "Hi, I'm Tiffany's ",
+                        StartDateTime: DateTime.Now,
+                        EndDateTime: DateTime.Now.AddHours(2),
+                        Image: new Uri("https://images.unsplash.com/photo-1620921575116-fb8902865f81?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"),
+                        Savory: new List<string> { "Sandwich", "Salad", "Omelet" },
+                        Sweet: new List<string> { "Pancake", "Waffle" }),        
+        };
+    }
+}
