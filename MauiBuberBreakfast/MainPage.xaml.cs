62d7b951acedbb671a6b6b8dfd017cd97ce6ddb8
swdev77 <swdev77@gmail.com>
2023-03-23 14:47:58 +0500

    Main View...

--- MauiBuberBreakfast/MainPage.xaml.cs
+++ MauiBuberBreakfast/MainPage.xaml.cs
@@ -1,12 +1,13 @@
-﻿namespace MauiBuberBreakfast;
+﻿using MauiBuberBreakfast.ViewModels;
+
+namespace MauiBuberBreakfast;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
+		BindingContext = new MyBreakfastViewModel();
 	}
 }
 
